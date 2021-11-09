a147ab3b58e4e6e62376bea492403dae4c752dca
Himanshu Agrahari <himanshu.a@osmosys.co>
2021-11-09 19:18:56 +0530

    Remove toggle button

--- handling_events/src/Components/Button.js
+++ handling_events/src/Components/Button.js
@@ -4,28 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class Button extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            isToggleOn: true
-        }
-
+        
         this.value = props.value
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick() {
-        this.setState(previousState => ({
-            isToggleOn: !previousState.isToggleOn // inverting previous state   
-        }));
+        
     }
 
     getRandomNumber(maxSize) {
         const max = Math.floor(maxSize);
-        return Math.floor(Math.random() * (max + 1));
+        return Math.floor(Math.random() * max);
     }
 
     getRandomClassName() {
         const classes = ['primary', 'secondary', 'success', 'danger', 'warning']
         const randNum = this.getRandomNumber(classes.length)
+        console.log(randNum)
+        console.log(classes[randNum])
         return classes[randNum]
     }
 
@@ -34,7 +31,7 @@ class Button extends React.Component {
         return (
             <button onClick={this.handleClick}
             className = {`btn btn-lg btn-block btn-${randomClassName}`}>
-                {this.state.isToggleOn ? this.value : randomClassName}
+                {randomClassName}
             </button>
         )
     }
