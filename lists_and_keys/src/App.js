f976f9a61ae9afa849239e33dcc86be753c9f23d
Himanshu Agrahari <himanshu.a@osmosys.co>
2021-11-10 19:12:34 +0530

    Render lists(multiple component)

--- lists_and_keys/src/App.js
+++ lists_and_keys/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 
 function App() {
+  const numbers = [1, 2, 3, 4, 5];
+  const listItems = numbers.map((number) =>
+    <li>{number}</li>
+  );
+
   return (
-    <h1>Hello World</h1>
+    <ul>
+      {listItems}
+    </ul>
   );
 }
 
